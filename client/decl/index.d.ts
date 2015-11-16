2dac33269a6620115f80e75d7dda43a30f69e212
Takehiro Iyatomi <iyatomi@gmail.com>
2015-11-16 16:50:48 +0900

    add js and proto.json also

---
+++ client/decl/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="../decl/main.d.ts" />
---
+++ client/www/js/index.js
@@ -0,0 +1,4 @@
+/// <reference path="../decl/main.d.ts"/>
+m.mount(document.body, new main.Component({
+    url: "ws://localhost:8888/ws"
+}));
